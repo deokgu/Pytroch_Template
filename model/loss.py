54254eddbf11871122f336b59466b625e7dbeda2
Deokgu <onefence1994@gmail.com>
2021-11-02 06:08:34 +0000

    add radm, dice_and fcal

--- model/loss.py
+++ model/loss.py
@@ -2,7 +2,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
-
+# F
 def nll_loss(output, target):
     return F.nll_loss(output, target)
 
@@ -104,3 +104,40 @@ class F1Loss(nn.Module):
         f1 = 2 * (precision * recall) / (precision + recall + self.epsilon)
         f1 = f1.clamp(min=self.epsilon, max=1 - self.epsilon)
         return 1 - f1.mean()
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.smooth = 1
+
+    def forward(self, input, target):
+        axes = tuple(range(1, input.dim()))
+        intersect = (input * target).sum(dim=axes)
+        union = torch.pow(input, 2).sum(dim=axes) + torch.pow(target, 2).sum(dim=axes)
+        loss = 1 - (2 * intersect + self.smooth) / (union + self.smooth)
+        return loss.mean()
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.eps = 1e-3
+
+    def forward(self, input, target):
+        input = input.clamp(self.eps, 1 - self.eps)
+        loss = - (target * torch.pow((1 - input), self.gamma) * torch.log(input) +
+                  (1 - target) * torch.pow(input, self.gamma) * torch.log(1 - input))
+        return loss.mean()
+
+
+class Dice_and_FocalLoss(nn.Module):
+    def __init__(self, gamma=2):
+        super(Dice_and_FocalLoss, self).__init__()
+        self.dice_loss = DiceLoss()
+        self.focal_loss = FocalLoss(gamma)
+
+    def forward(self, input, target):
+        loss = self.dice_loss(input, target) + self.focal_loss(input, target)
+        return loss
